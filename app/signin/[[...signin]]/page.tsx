58af31e422fbb58d71abbe5dd7b63557653737e3
Kakooza Vianey <kakoozavian@gmail.com>
2024-09-11 02:43:21 +0300

    debugging

--- app/signin/[[...signin]]/page.tsx
+++ app/signin/[[...signin]]/page.tsx
@@ -1,4 +1,4 @@
-import { SignIn } from '@clerk/nextjs';
+import { SignUp } from '@clerk/nextjs';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
 export default function SignInPage() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <SignIn appearance={{ variables: { colorPrimary: '#0F172A' } }} />
+      <SignUp appearance={{ variables: { colorPrimary: '#0F172A' } }} />
     </div>
   );
 }
