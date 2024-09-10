3ce03f6bdf0662c24823b208b6a884256d06ee5a
Kakooza Vianey <kakoozavian@gmail.com>
2024-09-11 02:52:25 +0300

    right routing

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,6 @@
 import { SignUp } from '@clerk/nextjs';
 import { Metadata } from 'next';
+import React from 'react';
 
 export const metadata: Metadata = {
   title: 'Decentracode - Sign In',
---
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,14 @@
+import { SignUp } from '@clerk/nextjs';
+import { Metadata } from 'next';
+import React from 'react';
+
+export const metadata: Metadata = {
+  title: 'Decentracode - Sign In',
+};
+export default function SignUpPage() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <SignUp appearance={{ variables: { colorPrimary: '#0F172A' } }} />
+    </div>
+  );
+}
